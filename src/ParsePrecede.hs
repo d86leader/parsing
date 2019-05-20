3890d361129edbb0023958db9c90ee2c55b0c61a
d86leader <d86leader@mail.com>
2019-05-21 01:22:12 +0700

    wrote precedence based parser

--- src/ParsePrecede.hs
+++ src/ParsePrecede.hs
@@ -1,5 +1,5 @@
 module ParsePrecede
-(
+( parse
 ) where
 
 
@@ -16,40 +16,57 @@ import Prelude hiding (lookup)
 
 parse :: HashMap (Symbol, Symbol) Prec
       -> Rules -> NonTerminal
-      -> [Symbol] -> String -> [Line]
-parse precedence rules start stack str =
+      -> String -> [(NonTerminal, Line)]
+parse precedence rules start str =
     let str' = map Term str
-    in if length rules /= length (HashMap.toList rules)
-       then error "Rules are not stratified"
-       else fromMaybe [] $ parse' [] str'
+    in fromMaybe [] $ parse' [] str'
     where
     --
     revRules = HashMap.fromList . join . map rev . HashMap.toList $ rules
     rev (nt, ls) = zip ls $ repeat nt
     --
-    parse' :: [Symbol] -> [Symbol] -> Maybe [Line]
+    recMaybe :: a -> Maybe [a] -> Maybe [a]
+    recMaybe _ Nothing = Nothing
+    recMaybe x (Just xs) = Just (x:xs)
+    --
+    parse' :: [Symbol] -> [Symbol] -> Maybe [(NonTerminal, Line)]
     parse' [] (first:str) = parse' [first] str
     parse' [start] []     = Just []
     parse' (top:stack) (first:str) =
         case precedence ! (top, first) of
             LeftPrec -> parse' (first:top:stack) str
             EqPrec ->   parse' (first:top:stack) str
-            RightPrec -> case fold (top:stack) of
-                           [] -> Nothing
-                           stack' -> parse' stack' (first:str)
-    parse' _ _ = Nothing
+            RightPrec ->
+                case fold (top:stack) of
+                    ([], _, _) -> Nothing
+                    (stack', nt, l) ->
+                        recMaybe (nt, l) $ parse' stack' (first:str)
+    parse' stack [] =
+        case fold stack of
+            ([], _, _) -> Nothing
+            (stack', nt, l) -> recMaybe (nt, l) $ parse' stack' []
     -- fold the stack according to precedence and rules
     -- empty fold signals error as any fold returns at least a nonterminal
-    fold :: [Symbol] -> [Symbol]
+    -- also return a rule used for folding
+    fold :: [Symbol] -> ([Symbol], NonTerminal, Line)
     fold stack =
         let (base, rest) = splitEqPrec stack
         in case lookup (Line base) revRules of
-            Nothing -> []
-            Just nt -> (Nonterm nt) : rest
+            Nothing -> ([], undefined, undefined)
+            Just nt -> let stack' = (Nonterm nt) : rest
+                       in (stack', nt, Line base)
     --
     eqPrec :: (Symbol, Symbol) -> Bool
     eqPrec (s1, s2) =
-        precedence ! (s1, s2) == EqPrec
+        case lookup (s2, s1) precedence of -- swapped because stack grows backwards
+            Just EqPrec -> True
+            otherwise -> False
+--         precedence ! (s1, s2) == EqPrec
     --
     splitEqPrec :: [Symbol] -> ([Symbol], [Symbol])
-    splitEqPrec = undefined
+    splitEqPrec syms =
+        let conseq = zip syms $ tail syms
+            (base', rest') = break (not . eqPrec) conseq
+            base = reverse $ head syms : map snd base'
+            rest = map snd rest'
+        in (base, rest)
